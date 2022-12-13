3d8991c689d9bb07e9bc90ed9ef772769001b999
CamileGhastine <camileghastine@hotmail.com>
2022-12-13 14:44:12 +0100

    post: refactor code

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,25 +7,19 @@ use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use DateTime;
-use Doctrine\Migrations\Configuration\EntityManager\ManagerRegistryEntityManager;
-use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(PostRepository $postRepository): Response
     {
-        // demander tous les posts au modèle et je vais les toscker dans la variable $posts
-        $posts = $postRepository->findAll();
-
         return $this->render('home/index.html.twig', [
-            'posts' => $posts
+            'posts' =>  $postRepository->findAll()
         ]);
     }
 
-    #[Route('/post/{id}')]
-    public function show(Post $post)
+    #[Route('/post/{id<[0-9]+>}')]
+    public function show(Post $post): Response
     {
         return $this->render('home/show.html.twig', [
             'post' => $post
