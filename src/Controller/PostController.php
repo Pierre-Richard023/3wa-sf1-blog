9dc54012e1b1b9b0a80a9e61a15c4a692d0ad098
CamileGhastine <camileghastine@hotmail.com>
2022-12-13 14:35:24 +0100

    post: display 1 article

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use DateTime;
 use Doctrine\Migrations\Configuration\EntityManager\ManagerRegistryEntityManager;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -20,6 +21,14 @@ class PostController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'posts' => $posts
+        ]);
+    }
+
+    #[Route('/post/{id}')]
+    public function show(Post $post)
+    {
+        return $this->render('home/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
